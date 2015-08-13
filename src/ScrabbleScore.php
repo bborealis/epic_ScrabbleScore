b916f4fbb29d55a667e0e8256faab0e8aac37a56
Brian Borealis & Charles Moss <student@epicodus.com>
2015-08-13 16:59:46 -0700

    5th test works

--- src/ScrabbleScore.php
+++ src/ScrabbleScore.php
@@ -1,25 +1,40 @@
 <?php
+
     Class ScrabbleScore {
 
         function wordScore ($word) {
 
-            // split
+              $word = strtoupper ($word);
+              $split_word = str_split($word);
+
+              $letters = array("A"=> 1, "E"=> 1, "I"=> 1, "O"=> 1, "U"=> 1, "L"=> 1, "N"=> 1, "R"=> 1, "S"=> 1, "T"=> 1, "D"=> 2, "G"=> 2,);
+
+              $score = 0;
+
+              foreach ($letters as $key=>$value) {
+                  if ((in_array($key, $split_word)) !==false){
+
+                      $score+= $value;
+
+                  }
+
+              }
+
+              if ($score === 0) {
+
+                      $score = "Please enter a letter.";
+
+              }
+
+              $sum_score = strval($score);
+
+                    return $sum_score;
+
+              }
 
 
 
-            $letters = array("A"=> 1, "E"=> 1, "I"=> 1, "O"=> 1, "U"=> 1, "L"=> 1, "N"=> 1, "R"=> 1, "S"=> 1, "T"=> 1);
+      }
 
-            foreach ($letters as $letter=>$point) {
-                if ($word === $letter) {
-                    $score = $point;
-                } else {
-                    $score = "Please enter a letter.";
-               }
-            }
-
-            $sum_score = strval($score);
-            return $sum_score;
-        }
-    }
 
 ?>
--- tests/ScrabbleScoreTest.php
+++ tests/ScrabbleScoreTest.php
@@ -4,7 +4,7 @@
 
     class ScrabbleScoreTest extends PHPUnit_Framework_TestCase {
 
-       // Enter "A" and get back "1".
+       //1. Enter "A" and get back "1".
        function test_wordScore_oneLetter() {
            $test_ScrabbleScore = new ScrabbleScore;
            $input = "A";
@@ -24,5 +24,41 @@
            $this->assertEquals("Please enter a letter.", $result);
        }
 
+       //3. Enter "a" and get back "1"."
+       function test_wordScore_lowerCaseLetter() {
+           $test_ScrabbleScore = new ScrabbleScore;
+           $input = "a";
+
+           $result = $test_ScrabbleScore->wordScore($input);
+
+           $this->assertEquals("1", $result);
+       }
+       //4. Enter "to" and get back "2".
+       function test_wordScore_twoLetters() {
+           $test_ScrabbleScore = new ScrabbleScore;
+           $input = "to";
+
+           $result = $test_ScrabbleScore->wordScore($input);
+
+           $this->assertEquals("2", $result);
+       }
+       //5. Enter "ad" and get back "3".
+       function test_wordScore_twoLettersHigherScore() {
+           $test_ScrabbleScore = new ScrabbleScore;
+           $input = "ad";
+
+           $result = $test_ScrabbleScore->wordScore($input);
+
+           $this->assertEquals("3", $result);
+       }
+       //6. Enter "adbfkjq" and get back "33".
+       function test_wordScore_sevenLettersHigherScore() {
+           $test_ScrabbleScore = new ScrabbleScore;
+           $input = "adbfkjq";
+
+           $result = $test_ScrabbleScore->wordScore($input);
+
+           $this->assertEquals("3", $result);
+       }
 }
 ?>
