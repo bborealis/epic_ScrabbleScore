f0904c050e516581d7653098a41ab90807f5d882
Brian Borealis & Charles Moss <student@epicodus.com>
2015-08-13 12:07:51 -0700

    first test pass

--- src/ScrabbleScore.php
+++ src/ScrabbleScore.php
@@ -3,6 +3,22 @@
 
         function wordScore ($word) {
 
+            // split
+
+
+
+            $letters = array("A"=> 1, "E"=> 1, "I"=> 1, "O"=> 1, "U"=> 1, "L"=> 1, "N"=> 1, "R"=> 1, "S"=> 1, "T"=> 1);
+
+            foreach ($letters as $letter=>$point) {
+                if ($word === $letter) {
+                    $score = $point;
+                }
+            }
+
+            $sum_score = strval($score);
+            return $sum_score;
+
+
         }
     }
 
